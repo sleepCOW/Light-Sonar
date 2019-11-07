51d795026daa7f9aeae32fccdfe8a3057b7f0763
sleepCOW <fgfg19817111@gmail.com>
2019-11-07 23:59:17 +0200

    scan_directory

--- Statistics/Statistics/Statistic_Functions.cpp
+++ Statistics/Statistics/Statistic_Functions.cpp
@@ -26,3 +26,18 @@ Lines count_lines(std::filesystem::path filePath) {
 
 	return count;
 }
+
+void scan_directory(std::filesystem::path directoryPath, Result& result)
+{
+	for (auto& iter : std::filesystem::directory_iterator(directoryPath)) {
+		if (iter.is_directory()) {
+			scan_directory(iter.path(), result);
+		}
+		else if (std::filesystem::path extension = iter.path().extension(); result == extension) {
+				auto [lines, blanks] = count_lines(iter.path());
+				result.addLines(extension, lines);
+				result.addBlanks(extension, blanks);
+				result.incrementFiles(extension);
+		}
+	}
+}
